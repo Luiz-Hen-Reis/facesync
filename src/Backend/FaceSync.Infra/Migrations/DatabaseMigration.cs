e3095cd04dbc3ad43136fc96b63fe06f628c2062
Luiz Henrique <lhcontato2020@gmail.com>
2026-05-21 20:21:42 -0300

    feat: Add database migration functionality and create UserFaces table

---
+++ src/Backend/FaceSync.Infra/Migrations/DatabaseMigration.cs
@@ -0,0 +1,56 @@
+﻿
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+
+namespace FaceSync.Infra.Migrations;
+
+public static class DatabaseMigration
+{
+    public static void Migrate(string connectionString, IServiceProvider serviceProvider)
+    {
+        EnsureDatabaseExists(connectionString);
+        MigrateDatabase(serviceProvider);
+    }
+
+    private static void EnsureDatabaseExists(string connectionString)
+    {
+        var connectionStringBuilder = new NpgsqlConnectionStringBuilder(connectionString);
+
+        var databaseName = connectionStringBuilder.Database;
+
+        // conecta no banco padrão do postgres
+        connectionStringBuilder.Database = "postgres";
+
+        using var connection = new NpgsqlConnection(connectionStringBuilder.ConnectionString);
+
+        connection.Open();
+
+        // verifica se o banco já existe
+        var checkDatabaseCommand = new NpgsqlCommand(
+            $"SELECT 1 FROM pg_database WHERE datname = '{databaseName}'",
+            connection
+        );
+
+        var databaseExists = checkDatabaseCommand.ExecuteScalar();
+
+        if (databaseExists is null)
+        {
+            var createDatabaseCommand = new NpgsqlCommand(
+                $"CREATE DATABASE \"{databaseName}\"",
+                connection
+            );
+
+            createDatabaseCommand.ExecuteNonQuery();
+        }
+    }
+
+    private static void MigrateDatabase(IServiceProvider serviceProvider)
+    {
+        var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+        runner.ListMigrations();
+
+        runner.MigrateUp();
+    }
+}
---
+++ src/Backend/FaceSync.Infra/Migrations/Versions/Version00000001.cs
@@ -0,0 +1,15 @@
+﻿using FluentMigrator;
+
+namespace FaceSync.Infra.Migrations.Versions;
+
+[Migration(1, "Create UserFaces table")]
+public class Version00000001 : ForwardOnlyMigration
+{
+    public override void Up()
+    {
+        Create.Table("UserFaces")
+            .WithColumn("Id").AsGuid().PrimaryKey().NotNullable()
+            .WithColumn("Name").AsString(200).NotNullable()
+            .WithColumn("Embeddings").AsCustom("real[]").NotNullable();
+    }
+}
